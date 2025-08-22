d1ec0e119d96e3cdb2c9529603fd0bb9a1b4f8c6
tomandrew1 <thomas.andrew.7@outlook.com>
2025-08-22 18:23:02 +0100

    attempted to optimise short and long MA, then checked with stocks instead of forex

--- get_data.py
+++ get_data.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 
 # Set the ticker as 'EURUSD=X'
-forex_data = yf.download('GBPUSD=X', period='2y', interval='1d')
+forex_data = yf.download('SPY', start='2020-01-01', end='2024-01-01', interval='1d')
 
 # Set the index to a datetime object
 forex_data.index = pd.to_datetime(forex_data.index)
@@ -21,83 +21,107 @@ print(forex_data.columns)
 forex_data = forex_data.drop(columns=["Volume"])
 
 
-plt.plot(forex_data.index, forex_data["Close"], label="Close")
-# plt.show()
-
-shortPeriod = 5
-longPeriod = 100
-
-
-# Creates a list of moving averages at each time interval starting at the periods' index
-shortMovingAverage = [sum(forex_data.iloc[i:i+shortPeriod,0])/shortPeriod for i in range(len(forex_data.index)-shortPeriod)]
-longMovingAverage = [sum(forex_data.iloc[i:i+longPeriod,0])/longPeriod for i in range(len(forex_data.index)-longPeriod)]
-
-plt.plot(forex_data.index[shortPeriod:], shortMovingAverage, label="SMA")
-plt.plot(forex_data.index[longPeriod:], longMovingAverage, label="LMA")
-plt.legend()
-plt.xticks(rotation=45)
+# plt.plot(forex_data.index, forex_data["Close"], label="Close")
 # plt.show()
 
 
-gbp = 1
-usd = 0
-print(gbp * forex_data.iloc[longPeriod, 0])
+shortVar = np.linspace(1, 51, 11)
+longVar = np.linspace(51, 351, 11)
+shortLat, longLat = np.meshgrid(shortVar, longVar)
 
-# initial conditions ie call api and calc avg once
-shortMovingAverage = shortMovingAverage[longPeriod - shortPeriod:]
-if shortMovingAverage[0] > longMovingAverage[0]:
-    shortPos = 1
-    
-else:
-    usd = gbp * forex_data.iloc[longPeriod, 0]
-    gbp = 0
-    shortPos = -1
-# previousPos = shortPos
+def calcMovingAverages(shortPeriod, longPeriod):
+    """ 
+    Creates a list of moving averages at each time interval starting at the periods' index
+    """
+    shortPeriod = int(shortPeriod)
+    longPeriod = int(longPeriod)
+    shortMovingAverage = [sum(forex_data.iloc[i:i+shortPeriod,0])/shortPeriod for i in range(len(forex_data.index)-shortPeriod)]
+    longMovingAverage = [sum(forex_data.iloc[i:i+longPeriod,0])/longPeriod for i in range(len(forex_data.index)-longPeriod)]
+    return shortMovingAverage, longMovingAverage
 
-bal = [[gbp, usd]]
-i = 1
-
-while i < len(longMovingAverage):
-    # call api
-    
-    # calc averages
-
-    # # check if we wanna buy / sell
-    # if shortMovingAverage[i] > longMovingAverage[i]:
-    #     shortPos = 1
-    # else:
-    #     shortPos = -1
-    
-    # # short moving below long ie selling usd into gbp
-    # if shortPos > previousPos:
-    #     gbp = usd / forex_data.iloc[i + longPeriod, 0]
-    #     usd = 0
-    # # short moving above long ie buying usd with gbp
-    # elif shortPos < previousPos:
-    #     usd = gbp * forex_data.iloc[i + longPeriod, 0]
-    #     gbp = 0
+# plt.plot(forex_data.index[shortPeriod:], shortMovingAverage, label="SMA")
+# plt.plot(forex_data.index[longPeriod:], longMovingAverage, label="LMA")
+# plt.legend()
+# plt.xticks(rotation=45)
+# plt.show()
 
 
-    if shortMovingAverage[i] > longMovingAverage[i] and shortPos==-1:
-        gbp = usd / forex_data.iloc[i + longPeriod, 0]
-        usd = 0
+def runSim(shortPeriod, longPeriod):
+    """
+    Runs the trading algorithm over the given time period
+    """
+    shortPeriod = int(shortPeriod)
+    longPeriod = int(longPeriod)
+
+    shortMovingAverage, longMovingAverage = calcMovingAverages(shortPeriod, longPeriod)
+
+    gbp = 1
+    usd = 0
+    # print(gbp, gbp * forex_data.iloc[longPeriod, 0])
+
+
+    # initial conditions ie call api and calc avg once
+    shortMovingAverage = shortMovingAverage[longPeriod - shortPeriod:]
+    if shortMovingAverage[0] > longMovingAverage[0]:
+        # usd = gbp * forex_data.iloc[longPeriod, 0]
+        # gbp = 0
         shortPos = 1
-    elif shortMovingAverage[i] < longMovingAverage[i] and shortPos==1:
-        usd = gbp * forex_data.iloc[i + longPeriod, 0]
+    else:
+        usd = gbp / forex_data.iloc[longPeriod, 0]
         gbp = 0
         shortPos = -1
 
-    # previousPos = shortPos
-    i += 1
-    
-    
-    bal.append([gbp, usd])
+    bal = [[gbp, usd]]
+    i = 1
 
-    # wait for new call for api
+    # print(len(shortMovingAverage), len(longMovingAverage))
+    # print(shortPeriod, longPeriod)
+    while i < len(longMovingAverage):
+        # call api
+        
+        # calc averages
 
-print(bal[-1][0], bal[-1][1] / forex_data.iloc[-1,0])
+        # BUY USD AT A HIGH, ie WHEN SHORT GOES ABOVE LONG
+        # SELL USD AT A LOW, ie WHEN SHORT GOES BELOW LONG
+        if shortMovingAverage[i] > longMovingAverage[i] and shortPos==-1:
+            # usd = gbp * forex_data.iloc[i + longPeriod, 0]
+            # gbp = 0
 
-plt.plot(forex_data.index[longPeriod:], [i[0] for i in bal], label="GBP")
-plt.plot(forex_data.index[longPeriod:], [i[1] for i in bal], label="USD")
+            gbp = usd * forex_data.iloc[i + longPeriod, 0]
+            usd = 0
+            
+            shortPos = 1
+        elif shortMovingAverage[i] < longMovingAverage[i] and shortPos==1:
+            # gbp = usd / forex_data.iloc[i + longPeriod, 0]
+            # usd = 0
 
+            usd = gbp / forex_data.iloc[i + longPeriod, 0]
+            gbp = 0
+            shortPos = -1
+
+        i += 1
+        
+        bal.append([gbp, usd])
+
+        # wait for new call for api
+
+        # print(bal[-1][0], bal[-1][1])
+
+    if bal[-1][0] == 0:
+        return bal[-1][1] / (forex_data.iloc[longPeriod][0])
+    else:
+        return bal[-1][0] 
+
+# plt.plot(forex_data.index[longPeriod:], [i[0] for i in bal], label="GBP")
+# plt.plot(forex_data.index[longPeriod:], [i[1] for i in bal], label="USD")
+
+# plt.show()
+
+
+runSimVect = np.vectorize(runSim)
+simResults = runSimVect(shortLat, longLat)
+plt.imshow(simResults, cmap="hot")
+plt.colorbar()
+plt.xticks(np.linspace(0,10,11), labels=shortVar, rotation=45)
+plt.yticks(np.linspace(0,10,11), labels=longVar)
 plt.show()
